613b32667c8abceec8c083c15a9ce4863f57c72c
IMPWNG <matisse.ach+github.com>
2022-05-09 17:56:52 -0400

    first

---
+++ SimpleTradingBot.py
@@ -0,0 +1,8 @@
+import time
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from wazirx_sapi_client.rest import Client
+import ccxt
+import talib
+from config import wazirx_config
